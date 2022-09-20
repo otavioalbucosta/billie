6f63cea41c386d0f2a2d79f7c0c17f7d27d15c94
otavioalbucosta <otavio-albucosta@hotmail.com>
2022-09-20 09:28:13 -0300

    new files

---
+++ billie/View/TotalOverView.swift
@@ -0,0 +1,47 @@
+//
+//  TotalOverView.swift
+//  billie
+//
+//  Created by Luciana Adrião on 19/09/22.
+//
+
+import SwiftUI
+
+struct TotalOverView: View {
+    
+    let totalPrice: Double
+    
+    var body: some View {
+            VStack(alignment: .leading) {
+                Group{
+                    HStack{
+                        Image(systemName: "person.fill")
+                        Text("aaaa")
+                        Spacer()
+                        Text("R$ \(totalPrice, specifier: "%.2f")")
+                    }
+                    .font(Font.title3.bold())
+                    Group {
+                        HStack{
+                            Image(systemName: "star")
+                            Text("aaaa")
+                            Spacer()
+                            Text("R$ \(totalPrice, specifier: "%.2f")")
+                        }
+                        HStack{
+                            Image(systemName: "star")
+                            Text("aaaa")
+                            Spacer()
+                            Text("R$ \(totalPrice, specifier: "%.2f")")
+                        }
+                    }.padding([.leading,.trailing],10).foregroundColor(.secondary)
+                }
+            }
+    }
+}
+
+struct TotalOverView_Previews: PreviewProvider {
+    static var previews: some View {
+        TotalOverView(totalPrice: 20)
+    }
+}
